0765242ca21fefac683367dd14dcfd6c61d4f6ce
jannik <jannikhodes@gmx.de>
2022-09-02 02:35:52 +0200

    setting up screen capture with windows, linux version still fastest

--- window_capture.py
+++ window_capture.py
@@ -3,9 +3,10 @@ import sys
 import numpy as np
 import cv2 as cv
 import pyautogui
-# import win32gui
-# import win32ui
-# import win32con
+import PIL
+import win32gui
+import win32ui
+import win32con
 
 from mss import mss
 from time import time
@@ -42,13 +43,18 @@ def fast_capture():
                 cv.destroyAllWindows()
                 break
 
-    '''
-    def fast_capture_win():
-        w = 1920  # set this
-        h = 1080  # set this
-        bmpfilenamename = "out.bmp"  # set this
-    
-        hwnd = win32gui.FindWindow(None, windowname)
+
+# fast capture for windows (still slower)
+def capture_win():
+    w, h = 1280, 720
+    start_time = time()
+    while{True}:
+        last_time = time()
+        # for now we will set hwnd to None to capture the primary monitor
+        # hwnd = win32gui.FindWindow(None, window_name)
+        hwnd = None
+
+    # get the window image data
         wDC = win32gui.GetWindowDC(hwnd)
         dcObj = win32ui.CreateDCFromHandle(wDC)
         cDC = dcObj.CreateCompatibleDC()
@@ -56,15 +62,36 @@ def fast_capture():
         dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
         cDC.SelectObject(dataBitMap)
         cDC.BitBlt((0, 0), (w, h), dcObj, (0, 0), win32con.SRCCOPY)
-        dataBitMap.SaveBitmapFile(cDC, bmpfilenamename)
-        # Free Resources
+
+        # convert the raw data into a format opencv can read
+        signedIntsArray = dataBitMap.GetBitmapBits(True)
+        img = np.frombuffer(signedIntsArray, dtype='uint8')
+        img.shape = (h, w, 4)
+
+        # free resources
         dcObj.DeleteDC()
         cDC.DeleteDC()
         win32gui.ReleaseDC(hwnd, wDC)
         win32gui.DeleteObject(dataBitMap.GetHandle())
-    '''
+
+        # drop the alpha channel to work with cv.matchTemplate()
+        img = img[..., :3]
+
+        # make image C_CONTIGUOUS to avoid errors with cv.rectangle()
+        img = np.ascontiguousarray(img)
+        print('FPS: {0}'.format(1 / (time()-last_time)))
+        cv.imshow('Steam', img)
+        if cv.waitKey(1) == ord('q'):
+            cv.destroyAllWindows()
+            break
+
+
+def win_enum_handler(hwnd, ctx):
+    if win32gui.IsWindowVisible(hwnd):
+        print(hex(hwnd), win32gui.GetWindowText(hwnd))
 
 
 if __name__ == '__main__':
-    #fast_capture()
-    slow_capture()
+    #capture_win()
+    fast_capture()
+    #slow_capture()
