11af7586d7a4a4115b2f086c83e0b405db9f6d1f
jannik <jannikhodes@gmx.de>
2022-09-02 23:00:46 +0200

    setting up screen capture with windows, linux version still fastest

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-if __name__ == '__main__':
-    pass
+import cv2 as cv
+import numpy as np
+import os
+from time import time
+from window_capture import WindowCapture
+
+
+# initialize the WindowCapture class
+wincap = WindowCapture('LOST ARK (64-bit, DX11) v.2.6.0.1')
+
+loop_time = time()
+while(True):
+
+    # get an updated image of the game
+    screenshot = wincap.get_screenshot()
+
+    cv.imshow('Lost Ark Bot', screenshot)
+
+    # debug the loop rate
+    print('FPS {}'.format(1 / (time() - loop_time)))
+    loop_time = time()
+
+    # press 'q' with the output window focused to exit.
+    # waits 1 ms every loop to process key presses
+    if cv.waitKey(1) == ord('q'):
+        cv.destroyAllWindows()
+        break
+
+print('Done.')
--- window_capture.py
+++ window_capture.py
@@ -1,20 +1,108 @@
-import os
-import sys
-
-import cv2
 import numpy as np
 import cv2 as cv
 import pyautogui
-import Xlib
-from PIL import ImageGrab
-#import win32gui
-#import win32ui
-#import win32con
-
+import win32gui
+import win32ui
+import win32con
 from mss import mss
 import time
 from ewmh import EWMH
-import random as rd
+
+
+import numpy as np
+import win32gui, win32ui, win32con
+
+
+class WindowCapture:
+
+    # properties
+    w = 0
+    h = 0
+    hwnd = None
+    cropped_x = 0
+    cropped_y = 0
+    offset_x = 0
+    offset_y = 0
+
+    # constructor
+    def __init__(self, window_name):
+        # find the handle for the window we want to capture
+        self.hwnd = win32gui.FindWindow(None, window_name)
+        if not self.hwnd:
+            raise Exception('Window not found: {}'.format(window_name))
+
+        # get the window size
+        window_rect = win32gui.GetWindowRect(self.hwnd)
+        self.w = window_rect[2] - window_rect[0]
+        self.h = window_rect[3] - window_rect[1]
+
+        # account for the window border and titlebar and cut them off
+        border_pixels = 8
+        titlebar_pixels = 30
+        self.w = self.w - (border_pixels * 2)
+        self.h = self.h - titlebar_pixels - border_pixels
+        self.cropped_x = border_pixels
+        self.cropped_y = titlebar_pixels
+
+        # set the cropped coordinates offset so we can translate screenshot
+        # images into actual screen positions
+        self.offset_x = window_rect[0] + self.cropped_x
+        self.offset_y = window_rect[1] + self.cropped_y
+
+    def get_screenshot(self):
+
+        # get the window image data
+        wDC = win32gui.GetWindowDC(self.hwnd)
+        dcObj = win32ui.CreateDCFromHandle(wDC)
+        cDC = dcObj.CreateCompatibleDC()
+        dataBitMap = win32ui.CreateBitmap()
+        dataBitMap.CreateCompatibleBitmap(dcObj, self.w, self.h)
+        cDC.SelectObject(dataBitMap)
+        cDC.BitBlt((0, 0), (self.w, self.h), dcObj, (self.cropped_x, self.cropped_y), win32con.SRCCOPY)
+
+        # convert the raw data into a format opencv can read
+        #dataBitMap.SaveBitmapFile(cDC, 'debug.bmp')
+        signedIntsArray = dataBitMap.GetBitmapBits(True)
+        img = np.fromstring(signedIntsArray, dtype='uint8')
+        img.shape = (self.h, self.w, 4)
+
+        # free resources
+        dcObj.DeleteDC()
+        cDC.DeleteDC()
+        win32gui.ReleaseDC(self.hwnd, wDC)
+        win32gui.DeleteObject(dataBitMap.GetHandle())
+
+        # drop the alpha channel, or cv.matchTemplate() will throw an error like:
+        #   error: (-215:Assertion failed) (depth == CV_8U || depth == CV_32F) && type == _templ.type()
+        #   && _img.dims() <= 2 in function 'cv::matchTemplate'
+        img = img[...,:3]
+
+        # make image C_CONTIGUOUS to avoid errors that look like:
+        #   File ... in draw_rectangles
+        #   TypeError: an integer is required (got type tuple)
+        # see the discussion here:
+        # https://github.com/opencv/opencv/issues/14866#issuecomment-580207109
+        img = np.ascontiguousarray(img)
+
+        return img
+
+    # find the name of the window you're interested in.
+    # once you have it, update window_capture()
+    # https://stackoverflow.com/questions/55547940/how-to-get-a-list-of-the-name-of-every-open-window
+    def list_window_names(self):
+        def winEnumHandler(hwnd, ctx):
+            if win32gui.IsWindowVisible(hwnd):
+                print(hex(hwnd), win32gui.GetWindowText(hwnd))
+        win32gui.EnumWindows(winEnumHandler, None)
+
+    # translate a pixel position on a screenshot image to a pixel position on the screen.
+    # pos = (x, y)
+    # WARNING: if you move the window being captured after execution is started, this will
+    # return incorrect coordinates, because the window position is only calculated in
+    # the __init__ constructor.
+    def get_screen_position(self, pos):
+        return (pos[0] + self.offset_x, pos[1] + self.offset_y)
+
 
 # slowest screen capture with pyautogui
 def slow_screen_capture():
@@ -31,7 +119,7 @@ def slow_screen_capture():
             break
 
 
-def fast_screen_capture():
+def fast_screen_capture_linux():
     top = 0
     left = 0
     width = 600
@@ -49,71 +137,26 @@ def fast_screen_capture():
             if cv.waitKey(1) == ord('q'):
                 cv.destroyAllWindows()
                 break
-'''
+
+
 # fast capture for windows (still slower)
-def capture_win():
-    w, h = 1280, 720
-    start_time = time()
-    while{True}:
-        last_time = time()
-        # for now we will set hwnd to None to capture the primary monitor
-        # hwnd = win32gui.FindWindow(None, window_name)
-        hwnd = None
-
-    # get the window image data
-        wDC = win32gui.GetWindowDC(hwnd)
-        dcObj = win32ui.CreateDCFromHandle(wDC)
-        cDC = dcObj.CreateCompatibleDC()
-        dataBitMap = win32ui.CreateBitmap()
-        dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
-        cDC.SelectObject(dataBitMap)
-        cDC.BitBlt((0, 0), (w, h), dcObj, (0, 0), win32con.SRCCOPY)
-
-        # convert the raw data into a format opencv can read
-        signedIntsArray = dataBitMap.GetBitmapBits(True)
-        img = np.frombuffer(signedIntsArray, dtype='uint8')
-        img.shape = (h, w, 4)
-
-        # free resources
-        dcObj.DeleteDC()
-        cDC.DeleteDC()
-        win32gui.ReleaseDC(hwnd, wDC)
-        win32gui.DeleteObject(dataBitMap.GetHandle())
-
-        # drop the alpha channel to work with cv.matchTemplate()
-        img = img[..., :3]
-
-        # make image C_CONTIGUOUS to avoid errors with cv.rectangle()
-        img = np.ascontiguousarray(img)
-        print('FPS: {0}'.format(1 / (time()-last_time)))
-        cv.imshow('Steam', img)
-        if cv.waitKey(1) == ord('q'):
-            cv.destroyAllWindows()
-            break
-
 
 def win_enum_handler(hwnd, ctx):
     if win32gui.IsWindowVisible(hwnd):
         print(hex(hwnd), win32gui.GetWindowText(hwnd))
-'''
+
 
 # selects needed window for screen capture
-def select_window():
+def select_window_linux():
     ewmh = EWMH()
     wins = ewmh.getClientList()
     win = ewmh.getActiveWindow()
     for i in wins:
         name = ewmh.getWmName(i)
-        #print(name)
         if str(name).__contains__('Mozilla'):  # replace with la name
             ewmh.setMoveResizeWindow(i, 1, 0, 0, 600, 400)
             ewmh.setActiveWindow(i)
             ewmh.display.flush()
-        #print(name)
 
 
-if __name__ == '__main__':
-    select_window()
-    #capture_screen_win()
-    fast_screen_capture()
-    #slow_screen_capture()
+
