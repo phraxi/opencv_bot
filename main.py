075892c5e34c5991c841dcb03e9f086a766ab4ac
jannik <jannikhodes@gmx.de>
2022-09-28 22:44:48 +0200

    added window name as param of the WindowCapture class, fixed name issue after update, outsourced some code, setting up opencv tools for both win and lin

--- main.py
+++ main.py
@@ -7,36 +7,49 @@ from time import time
 from window_capture import WindowCapture
 
 
+# utility function to count all files in dir
 def count_files_in_dir(di):
     return len([entry for entry in os.listdir(di) if os.path.isfile(os.path.join(di, entry))])
 
 
-#WindowCapture.list_window_names(None)
-wincap = WindowCapture('LOST ARK (64-bit, DX11) v.2.7.1.1')
-loop_time = time()
-while (True):
+# method to find the window of LA (or other app) that needs to be captured
+def find_la_win():
+    wins = WindowCapture.list_window_names()
+    for i in wins:
+        if i.__contains__('LOST ARK'):
+            la_win = WindowCapture(i)
+            print(i)
+    return la_win
 
-    # get an updated image of the game
-    screenshot = wincap.get_screenshot()
-    cv.imshow('Lost Ark Bot', screenshot)
 
-    # debug the loop rate
-    print('FPS {}'.format(1 / (time() - loop_time)))
+# initializes screen capture of the chosen window and renders images every second to an
+# open cv window
+def ini_capture(window):
     loop_time = time()
+    while True:
+        # get an updated image of the game
+        screenshot = window.fast_screen_cap_win()
+        cv.imshow('Lost Ark Bot', screenshot)
+        # print('FPS {}'.format(1 / (time() - loop_time)))
+        loop_time = time()
+        pos = 'pos'
+        neg = 'neg'
 
-    pos = 'pos'
-    neg = 'neg'
+        # waits 1 ms every loop to process key presses
+        key = cv.waitKey(1)
+        # press 'q' with the output window focused to exit.
+        if key == ord('q'):
+            cv.destroyAllWindows()
+            break
+        # press 'p' to take a positive screenshot and write it in pos dir
+        elif key == ord('p'):
+            cv.imwrite('pos/{}.jpg'.format(loop_time), screenshot)
+        # press 'n' to take a negative screenshot and write it in neg dir
+        elif key == ord('n'):
+            cv.imwrite('neg/{}.jpg'.format(loop_time), screenshot)
+    # print("Positive Screenshots: ", count_files_in_dir(pos))
+    # print("Negative Screenshots: ", count_files_in_dir(neg))
 
-    # press 'q' with the output window focused to exit.
-    # waits 1 ms every loop to process key presses
-    key = cv.waitKey(1)
-    if key == ord('q'):
-        cv.destroyAllWindows()
-        break
-    elif key == ord('p'):
-        cv.imwrite('pos/{}.jpg'.format(loop_time), screenshot)
-    elif key == ord('n'):
-        cv.imwrite('neg/{}.jpg'.format(loop_time), screenshot)
-print("Positive Screenshots: ", count_files_in_dir(pos))
-print("Negative Screenshots: ", count_files_in_dir(neg))
 
+win = find_la_win()
+ini_capture(win)
--- window_capture.py
+++ window_capture.py
@@ -10,7 +10,6 @@ from ewmh import EWMH
 
 
 class WindowCapture:
-
     # properties
     w = 0
     h = 0
@@ -19,11 +18,13 @@ class WindowCapture:
     cropped_y = 0
     offset_x = 0
     offset_y = 0
+    window_name = None
 
     # constructor
     def __init__(self, window_name):
         # find the handle for the window we want to capture
         self.hwnd = win32gui.FindWindow(None, window_name)
+        self.window_name = window_name
         if not self.hwnd:
             raise Exception('Window not found: {}'.format(window_name))
 
@@ -45,7 +46,8 @@ class WindowCapture:
         self.offset_x = window_rect[0] + self.cropped_x
         self.offset_y = window_rect[1] + self.cropped_y
 
-    def get_screenshot(self):
+    # fast capture on Windows
+    def fast_screen_cap_win(self):
 
         # get the window image data
         wDC = win32gui.GetWindowDC(self.hwnd)
@@ -67,30 +69,21 @@ class WindowCapture:
         cDC.DeleteDC()
         win32gui.ReleaseDC(self.hwnd, wDC)
         win32gui.DeleteObject(dataBitMap.GetHandle())
-
-        # drop the alpha channel, or cv.matchTemplate() will throw an error like:
-        #   error: (-215:Assertion failed) (depth == CV_8U || depth == CV_32F) && type == _templ.type()
-        #   && _img.dims() <= 2 in function 'cv::matchTemplate'
         img = img[..., :3]
-
-        # make image C_CONTIGUOUS to avoid errors that look like:
-        #   File ... in draw_rectangles
-        #   TypeError: an integer is required (got type tuple)
-        # see the discussion here:
-        # https://github.com/opencv/opencv/issues/14866#issuecomment-580207109
         img = np.ascontiguousarray(img)
-
         return img
 
-    # find the name of the window you're interested in.
-    # once you have it, update window_capture()
-    # https://stackoverflow.com/questions/55547940/how-to-get-a-list-of-the-name-of-every-open-window
-    def list_window_names(self):
-        def winEnumHandler(hwnd, ctx):
-            if win32gui.IsWindowVisible(hwnd):
-                print(hex(hwnd), win32gui.GetWindowText(hwnd))
+    # Method to return a list with the names of all opened windows
+    @staticmethod
+    def list_window_names():
+        li = []
 
-        win32gui.EnumWindows(winEnumHandler, None)
+        def win_enum_handler(hwnd, ctx):
+            if win32gui.IsWindowVisible(hwnd):
+                li.append(win32gui.GetWindowText(hwnd))
+
+        win32gui.EnumWindows(win_enum_handler, None)
+        return li
 
     # translate a pixel position on a screenshot image to a pixel position on the screen.
     # pos = (x, y)
@@ -98,25 +91,11 @@ class WindowCapture:
     # return incorrect coordinates, because the window position is only calculated in
     # the __init__ constructor.
     def get_screen_position(self, pos):
-        return (pos[0] + self.offset_x, pos[1] + self.offset_y)
+        return pos[0] + self.offset_x, pos[1] + self.offset_y
 
-
-# slowest screen capture with pyautogui
-def slow_screen_capture():
-    loop_time = time.time()
-    while (True):
-        screenshot = pyautogui.screenshot()
-        screenshot = np.array(screenshot)
-        screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
-        cv.imshow('Hi', screenshot)
-        print('FPS{}'.format(1 / (time.time() - loop_time)))
-        loop_time = time.time()
-        if cv.waitKey(1) == ord('q'):
-            cv.destroyAllWindows()
-            break
-
-
-def fast_screen_capture_linux():
+'''
+# Things for Linux can be ignored
+def fast_screen_cap_lin():
     top = 0
     left = 0
     width = 600
@@ -136,16 +115,8 @@ def fast_screen_capture_linux():
                 break
 
 
-# fast capture for windows (still slower)
-
-@staticmethod
-def win_enum_handler(hwnd, ctx):
-    if win32gui.IsWindowVisible(hwnd):
-        print(hex(hwnd), win32gui.GetWindowText(hwnd))
-
-
 # selects needed window for screen capture
-def select_window_linux():
+def select_window_lin():
     ewmh = EWMH()
     wins = ewmh.getClientList()
     win = ewmh.getActiveWindow()
@@ -155,3 +126,7 @@ def select_window_linux():
             ewmh.setMoveResizeWindow(i, 1, 0, 0, 600, 400)
             ewmh.setActiveWindow(i)
             ewmh.display.flush()
+'''
+
+
+
